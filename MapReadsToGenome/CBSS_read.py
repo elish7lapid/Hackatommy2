aa4018fa7eefeda5bdaa442747ae94c06f41ee20
sinmaker <sinmaker@users.noreply.github.com>
2018-07-12 10:57:07 +0300

    Returning to a working copy

--- MapReadsToGenome/CBSS_read.py
+++ MapReadsToGenome/CBSS_read.py
@@ -5,9 +5,6 @@ import numpy as np
 name_to_index ={"QNAME":0, "FLAG":1, "RNAME":2, "POS":3, "MAPQ":4, "CIGAR":5, "RNEXT":6, "PNEXT":7, "TLEN":8, "SEQ":9,
                                   "QUAL":10,
                                   "*":11, " DIFF":12, "METH":13, "**":14}
-
-
-THRESHOLD_PRECENT = 0.25
 class CBSS_read:
 
    def __init__(self, chr, pos, seq, meth):
@@ -15,16 +12,13 @@ class CBSS_read:
        self.pos = int(pos)
        self.seq = seq
        self.methylation = meth
-       self.countMethylation()
+
 
 
    def countMethylation(self):
        num_z = self.methylation.count("z")
        num_Z = self.methylation.count("Z")
-       if (num_z == 0 and num_Z == 0):
-           self.methy_precentage = 0
-       else:
-           self.methy_precentage =num_Z /(num_z + num_Z  ) # new field)
+       self.methy_precentage =num_Z /(num_z + num_Z  ) # new field)
        self.methy_count = num_Z
        self.unmethy_count =num_z   # new field
 
@@ -34,32 +28,16 @@ def create_reads_vector(filename):
     # table_file = pd.read_table(filename,header = None, sep = "\t", index_col=False)
     # total_rows = table_file.shape[0]
 
-
-    meth_SAM_file = open(r"D:\LEA\BIOINFORMATICS\Year_3\Sem_B\TOMMY2\HAckatommy\methylated_SAM", "w")
-    unmeth_SAM_file = open(r"D:\LEA\BIOINFORMATICS\Year_3\Sem_B\TOMMY2\HAckatommy\unmethylated_SAM", "w")
     reads_list = []
     lines = file.readlines()
     for line in lines:
         parsed = line.split()
         chr = parsed[name_to_index["RNAME"]]
-        pos = int(parsed[name_to_index["POS"]])
+        pos = parsed[name_to_index["POS"]]
         seq = parsed[name_to_index["SEQ"]]
         meth = parsed[name_to_index["METH"]]
         cbss_Read = CBSS_read(chr, pos, seq, meth)
-        cbss_Read.countMethylation()
-
-        end = pos+len(seq)
-        if cbss_Read.methy_precentage <THRESHOLD_PRECENT:
-            unmeth_SAM_file.write(line)
-        else:
-            meth_SAM_file.write(line)
-
-
-
-
         reads_list.append(cbss_Read)
 
     reads_vector = np.array(reads_list)
     return reads_vector
-
-create_reads_vector(r"D:\LEA\BIOINFORMATICS\Year_3\Sem_B\TOMMY2\HAckatommy\cat_H3K36me3_2D.sam_ch9.unique")
